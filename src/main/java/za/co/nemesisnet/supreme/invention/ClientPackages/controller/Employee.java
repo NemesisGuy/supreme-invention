519d8f5a3123e4d7a6677e893628cffc511437e4
Reign Buckingham <24620376+NemesisGuy@users.noreply.github.com>
2022-09-28 11:55:41 +0200

    commit Client

---
+++ src/main/java/za/co/nemesisnet/supreme/invention/ClientPackages/controller/Employee.java
@@ -0,0 +1,30 @@
+package za.co.nemesisnet.supreme.invention.ClientPackages.controller;
+import java.io.*;
+import java.util.*;
+
+public class Employee implements Serializable {
+
+    private int employeeNumber;
+    private String employeeName;
+
+    Employee(int num, String name) {
+        employeeNumber = num;
+        employeeName= name;
+    }
+
+    public int getEmployeeNumber() {
+        return employeeNumber ;
+    }
+
+    public void setEmployeeNumber(int num) {
+        employeeNumber = num;
+    }
+
+    public String getEmployeeName() {
+        return employeeName ;
+    }
+
+    public void setEmployeeName(String name) {
+        employeeName = name;
+    }
+}
